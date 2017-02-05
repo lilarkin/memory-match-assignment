6cf1a90c40c9c63a159844bf90a3e239bc75ffd8
Lindsay Larkin Larkin <lindsay.larkin@gmail.com>
2017-02-05 14:55:35 -0700

    remove images from HTML and try to put them in array in memory_match.js

--- mm_assets/mm_javascripts/memory_match.js
+++ mm_assets/mm_javascripts/memory_match.js
@@ -1,11 +1,20 @@
 $(document).ready(function() {
   
+  // var tiles : [ 'mm_assets/mm_images/img1.JPG', 
+  //             'mm_assets/mm_images/img2.JPG', 
+  //             'mm_assets/mm_images/img3.JPG',
+  //             'mm_assets/mm_images/img4.JPG',
+  //             'mm_assets/mm_images/img5.JPG',
+  //             'mm_assets/mm_images/img6.JPG',
+  //             'mm_assets/mm_images/img7.JPG',
+  //             'mm_assets/mm_images/img8.JPG']
+
   var $button = $('.btn');
 
 
 // make this do the opposite, so white and shows image if clicked
   $button.on('click', function() {
-    $(this).find('img').hide();
+    $(this).find('img').show();
     //if match {
       //face up
       //alert('You found a match!')
@@ -22,5 +31,10 @@ $(document).ready(function() {
   // quit or reshuffle 
   // alert if pick a card that is showing (already matched)
 
+  // onGameStart : function() { return false; },
+  // onGameEnd : function() { return false; }
+
+  // var myMem = new Memory;
+  // myMem.resetGame();
 
 });
